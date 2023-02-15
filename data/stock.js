9156966d02ddefcac601090a00b53822797f9fdf
BritosRM <rodrixeneize00@gmail.com>
2023-02-15 16:58:53 -0300

    primer commit

---
+++ data/stock.js
@@ -0,0 +1,33 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Cable 1",
+        tipo: "baja",
+        desc: "Cable de baja tension por metro",
+        precio: 600,
+        imagen: '../public/images/baja.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Cable 2",
+        tipo: "media",
+        desc: "Cable de media tension por metro",
+        precio: 1000,
+        imagen: '../public/images/media.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Cable 3",
+        tipo: "alta",
+        desc: "Cable de alta tension por metro",
+        precio: 2000,
+        imagen: '../public/images/alta.jpg',
+        cantidad:1
+    },
+
+];
